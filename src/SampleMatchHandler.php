0be399c83acca4b38d6087140badd4a72c8f9f16
Alejo Lucangeli <alejolucangeli@gmail.com>
2017-01-17 18:32:36 -0300

    Refactor: SampleMatchHandler doesn't echo anything, it just returns

--- src/SampleMatchHandler.php
+++ src/SampleMatchHandler.php
@@ -6,16 +6,16 @@ class SampleMatchHandler {
 
     public function handle(Match $match) {
         if (!$match->isMatch()) {
-            echo '<h1>404 NOT FOUND</h1>';
+            return '<h1>404 NOT FOUND</h1>';
         } elseif (!$match->filtersPass()) {
-            echo 'Filter error: ' . $match->getFilterError() . '<br />';
+            return 'Filter error: ' . $match->getFilterError() . '<br />';
         } else {
             $result = $match->getResult();
 
             if (is_string($result)) {
-                echo $result;
+                return $result;
             } elseif (is_callable($result)) {
-                echo call_user_func_array($result, $match->getParameters());
+                return call_user_func_array($result, $match->getParameters());
             } else {
                 throw new Exception('Unhandled match type: ' . gettype($result));
             }
