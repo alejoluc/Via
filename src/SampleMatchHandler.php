a6a1589dc1e211e4ad4f4ecfaa0c4dc6b5d8bccc
Alejo Lucangeli <alejolucangeli@gmail.com>
2017-01-18 14:14:20 -0300

    Refactor: SampleMatchHandler

--- src/SampleMatchHandler.php
+++ src/SampleMatchHandler.php
@@ -8,16 +8,40 @@ class SampleMatchHandler {
         if (!$match->isMatch()) {
             return '<h1>404 NOT FOUND</h1>';
         } elseif (!$match->filtersPass()) {
-            return 'Filter error: ' . $match->getFilterError() . '<br />';
+            $errors = $match->getFilterErrors();
+            $ret = '<h1>Some filters reported an error:</h1>';
+            $ret .= '<ul>';
+            foreach ($errors as $error) {
+                $ret .= '<li>' . $error . '</li>';
+            }
+            $ret .= '</ul>';
+            return $ret;
         } else {
             $result = $match->getResult();
 
             if (is_string($result)) {
                 return $result;
+            } elseif (is_array($result)) {
+                if (count($result) < 2) {
+                    throw new \InvalidArgumentException('Route must return an array of at least two elements:
+                    controller and method names');
+                }
+
+                $controller = $result[0];
+                $method     = $result[1];
+                if (!class_exists($controller)) {
+                    throw new \Exception('Class ' . $controller . ' could not be found');
+                }
+                if (!method_exists($controller, $method)) {
+                    throw new \Exception('Class ' . $controller . ' does not have a ' . $method . '() method');
+                }
+
+                $instance = new $controller();
+                return call_user_func_array([$instance, $method], $match->getParameters());
             } elseif (is_callable($result)) {
                 return call_user_func_array($result, $match->getParameters());
             } else {
-                throw new Exception('Unhandled match type: ' . gettype($result));
+                throw new \Exception('Unhandled match type: ' . gettype($result));
             }
         }
     }
